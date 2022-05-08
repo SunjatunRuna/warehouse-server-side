10c5044982b7113262b494bb630987fc0233aa44
SunjatunRuna <sunjatuna@gmail.com>
2022-05-08 19:09:27 +0600

    delete api

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ async function run() {
             const result = await productCollection.insertOne(addItem);
             res.send(result);
         })
+        app.delete('/product/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
